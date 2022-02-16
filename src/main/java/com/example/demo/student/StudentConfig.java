48e69e6e7b5b798b926b535fff67345637bfedfa
Japerre <tibolaperre@outlook.com>
2022-02-16 11:21:16 +0100

    database persistent gemaakt

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,32 +1,32 @@
-package com.example.demo.student;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
-@Configuration
-public class StudentConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner(StudentRepository repository) {
-        return args -> {
-            Student Tibo = new Student(
-                    "tibo",
-                    "tibolaperre@outlook.com",
-                    LocalDate.of(2001, Month.SEPTEMBER,9)
-            );
-
-            Student Emma = new Student(
-                    "emma",
-                    "emmaDeleu@outlook.com",
-                    LocalDate.of(2002, Month.OCTOBER,10)
-            );
-
-            repository.saveAll(List.of(Tibo, Emma));
-        };
-    }
-}
+//package com.example.demo.student;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.time.LocalDate;
+//import java.time.Month;
+//import java.util.List;
+//
+//@Configuration
+//public class StudentConfig {
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository repository) {
+//        return args -> {
+//            Student Tibo = new Student(
+//                    "tibo",
+//                    "tibolaperre@outlook.com",
+//                    LocalDate.of(2001, Month.SEPTEMBER,9)
+//            );
+//
+//            Student Emma = new Student(
+//                    "emma",
+//                    "emmaDeleu@outlook.com",
+//                    LocalDate.of(2002, Month.OCTOBER,10)
+//            );
+//
+//            repository.saveAll(List.of(Tibo, Emma));
+//        };
+//    }
+//}
